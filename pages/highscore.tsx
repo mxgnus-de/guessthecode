d1ac805fc713d620fe67e9b8839ede26e0a388dd
Mags007 <magnus.bache@outlook.de>
2022-02-19 18:58:33 +0100

    updated highscore.tsx

--- pages/highscore.tsx
+++ pages/highscore.tsx
@@ -64,7 +64,7 @@ export const getServerSideProps: GetServerSideProps<SiteProps> = async () => {
 
    return {
       props: {
-         highscores: error ? [] : reponse.data.highscores,
+         highscores: reponse.data.highscores,
       },
    };
 };
